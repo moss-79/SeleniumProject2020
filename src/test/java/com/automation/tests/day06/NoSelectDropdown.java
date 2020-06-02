15f728a4e7f84e741718ccf45ef2790a9c0308ed
moss-79 <mosskdori@gmail.com>
2020-06-02 13:01:32 -0500

    Drop - down SelectByIndex

--- src/test/java/com/automation/tests/day06/NoSelectDropdown.java
+++ src/test/java/com/automation/tests/day06/NoSelectDropdown.java
@@ -2,8 +2,12 @@ package com.automation.tests.day06;
 
 import com.automation.utilities.BrowserUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class NoSelectDropdown {
     public static void main(String[] args) {
@@ -13,9 +17,29 @@ public class NoSelectDropdown {
         driver.get("http://practice.cybertekschool.com/dropdown");
         BrowserUtils.wait(3);
 
+        driver.findElement(By.id("dropdownMenuLink")).click(); // to expand dropdown
+        BrowserUtils.wait(2);
+
+      //  <a class="dropdown-item" href="https://www.amazon.com/">Amazon</a>
+        driver.findElement(By.linkText("Amazon")).click();// click on option
+        driver.navigate().back();
+
+        // to get all the links you go back and do it like from the first
+        driver.findElement(By.id("dropdownMenuLink")).click(); // to expand dropdown
+        BrowserUtils.wait(2);
+        List<WebElement> links = driver.findElements(By.className("dropdown-item"));
+        for (WebElement link : links){
+         //   link.click();
+            System.out.println(link.getText()+ " : "  +link.getAttribute("href"));
+            BrowserUtils.wait(2);
+          //  driver.navigate().back();
+           // System.out.println();
+        }
 
 
 
+
+        BrowserUtils.wait(3 );
         driver.quit();
     }
 }
---
+++ src/test/java/com/automation/tests/day06/SelectByIndex.java
@@ -0,0 +1,18 @@
+package com.automation.tests.day06;
+
+import com.automation.utilities.BrowserUtils;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SelectByIndex {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://practice.cybertekschool.com/dropdown");
+        BrowserUtils.wait(3);
+
+
+    }
+}
